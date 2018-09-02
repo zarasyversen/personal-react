e31c15b57f18cc8d9c16cd80e254784e9dfe868b
Zara <zara@gene.co.uk>
2018-09-02 20:03:43 +0100

    add sass and nav page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import '../css/Nav.css';
 
 class Nav extends Component {
   render() {
     return (
-      <h1>I am nav</h1>
+	<div className="wrapper">
+	  	<nav className="nav__container">
+		    <div className="nav__item current">
+		      <h3>How I am spending my days right now</h3>
+		    </div>
+		    <div className="nav__item background">
+		      <h3>Where I am from</h3>
+		    </div>
+		    <div className="nav__item community">
+		      <h3>How much I love a good community like codebar</h3>
+		    </div>
+		    <div className="nav__item fun-stuff">
+		      <h3>Fun</h3>
+		    </div>
+		    <div className="nav__item proud-stuff">
+		      <h3>Things I am proud of</h3>
+		    </div>
+	  	</nav>
+  	</div>
     );
   }
 }
